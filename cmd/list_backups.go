fa7a42c7db22b07b96571e2e3ee1a1d3f867df0c
Cian Hatton <cianhatton@gmail.com>
2022-07-18 19:01:43 +0100

    use json output to display file information

--- cmd/list_backups.go
+++ cmd/list_backups.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path"
 	"path/filepath"
 	"regexp"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -39,28 +41,71 @@ var listBackupsCommand = &cobra.Command{
 	},
 }
 
-func cmdListBackups(hostDir string) error {
-	var list []string
-	err := filepath.Walk(hostDir, func(path string, info os.FileInfo, err error) error {
+// backedUpVolume holds information about a volume backup.
+type backedUpVolume struct {
+	VolumeName       string    `json:"volumeName"`
+	AbsoluteFilePath string    `json:"absoluteFilePath"`
+	FileName         string    `json:"fileName"`
+	LastModTime      time.Time `json:"lastModTime"`
+}
+
+func getAllVolumeBackups(hostDir string) ([]backedUpVolume, error) {
+	var result []backedUpVolume
+	err := filepath.Walk(hostDir, func(filePath string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
 		}
-		list = append(list, path)
+		fileName := path.Base(filePath)
+		if !rxp.Match([]byte(fileName)) {
+			return nil
+		}
+
+		match := rxp.FindAllStringSubmatch(fileName, -1)
+		dockerVolumeName := match[0][1]
+
+		result = append(result, backedUpVolume{
+			VolumeName:       dockerVolumeName,
+			AbsoluteFilePath: filePath,
+			LastModTime:      info.ModTime(),
+			FileName:         info.Name(),
+		})
+
 		return nil
 	})
+	if err != nil {
+		return nil, err
+	}
 
+	return result, nil
+}
+
+// cmdListBackups outputs a list of backups in the given directory.
+// e.g.
+/*
+[
+  {
+    "volumeName": "docker-volume-backup_config",
+    "absoluteFilePath": "/var/folders/k1/z859m4qx7ld12gdjfv_79tv80000gn/T/tmp.9a5CNbHp/docker-volume-backup_config-18-7-2022.tar.gz",
+    "fileName": "docker-volume-backup_config-18-7-2022.tar.gz",
+    "lastModTime": "2022-07-18T19:00:07.553274229+01:00"
+  },
+  {
+    "volumeName": "docker-volume-backup_metadata",
+    "absoluteFilePath": "/var/folders/k1/z859m4qx7ld12gdjfv_79tv80000gn/T/tmp.9a5CNbHp/docker-volume-backup_metadata-18-7-2022.tar.gz",
+    "fileName": "docker-volume-backup_metadata-18-7-2022.tar.gz",
+    "lastModTime": "2022-07-18T19:00:08.607626133+01:00"
+  }
+]
+*/
+func cmdListBackups(hostDir string) error {
+	result, err := getAllVolumeBackups(hostDir)
 	if err != nil {
 		return err
 	}
-
-	for _, fn := range list {
-		fileName := path.Base(fn)
-		if !rxp.Match([]byte(fileName)) {
-			continue
-		}
-		match := rxp.FindAllStringSubmatch(fileName, -1)
-		dockerVolumeName := match[0][1]
-		fmt.Println(dockerVolumeName)
+	bytes, err := json.Marshal(result)
+	if err != nil {
+		return err
 	}
+	fmt.Println(string(bytes))
 	return nil
 }
